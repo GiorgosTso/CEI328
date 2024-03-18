1733b02f85d571ba941fe3dc3a534e4e8f34c34e
AndreasChr03 <andreasggchristou@gmail.com>
2024-03-18 16:59:45 +0200

    login css

--- brber-master/php/login.php
+++ brber-master/php/login.php
@@ -120,19 +120,77 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>Login Page</title>
           <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
-          <link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"crossorigin="anonymous">
-          <link rel="stylesheet" href="login.css">
+          <link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"crossorigin="anonymous"> 
+            
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+          <link rel="manifest" href="site.webmanifest">
+          
+          <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../assets/css/slicknav.css">
+    <link rel="stylesheet" href="../assets/css/animate.min.css">
+    <link rel="stylesheet" href="../assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="../assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="../assets/css/themify-icons.css">
+    <link rel="stylesheet" href="../assets/css/themify-icons.css">
+    <link rel="stylesheet" href="../assets/css/slick.css">
+    <link rel="stylesheet" href="../assets/css/nice-select.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="login_123.css">
+    
+
+	<!-- CSS here -->
+	
+	<link rel="stylesheet" href="../assets/css/style.css">
+          
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
           
      </head>
-<body>
-
- 
-                   
-     
-  
-<form action= <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
+        <body>
+        
+        
+        <div id="preloader-active">
+                <div class="preloader d-flex align-items-center justify-content-center">
+                    <div class="preloader-inner position-relative">
+                        <div class="preloader-circle"></div>
+                        <div class="preloader-img pere-text">
+                            <img src="../assets/img/logo.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="header-area header-transparent pt-20">
+                <div class="main-header header-sticky">
+                    <div class="container-fluid">
+                        <div class="row align-items-center">
+                            <!-- Logo -->
+                            <div class="col-xl-2 col-lg-2 col-md-1">
+                                <div class="logo">
+                                    <img src="../assets/img/logo.png" alt=""></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+             
+             
+             <div class="slider-area2">
+                    <div class="slider-height2 d-flex align-items-center">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-xl-12">
+                                    <div class="hero-cap hero-cap2 pt-70 text-center">
+                                        <h2>Login</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        <div class="allForm">
+        <form action= <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
      <h1>Login to your account</h1>
           <div class="form-group">
                <label for="exampleInputEmail1">Email address</label>
@@ -157,7 +215,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     #togglePassword {
           position: relative;
           bottom: 32px;
-          left: 405px;
+          left: 350px;
           cursor: pointer;
           color: black; /* Default color */
     }
@@ -178,6 +236,49 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                Do you want to create an account ? <a href="createAccount.php">Signup</a>
           </div>
      </form>
+        </div>
+        
+
+     
+     
+    <script src="../assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="../assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="../assets/js/popper.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="../assets/js/jquery.slicknav.min.js"></script>
+
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="../assets/js/owl.carousel.min.js"></script>
+    <script src="../assets/js/slick.min.js"></script>
+    <!-- One Page, Animated-HeadLin -->
+    <script src="../assets/js/wow.min.js"></script>
+    <script src="../assets/js/animated.headline.js"></script>
+    <script src="../assets/js/jquery.magnific-popup.js"></script>
+
+    <!-- Date Picker -->
+    <script src="../assets/js/gijgo.min.js"></script>
+    <!-- Nice-select, sticky -->
+    <script src="../assets/js/jquery.nice-select.min.js"></script>
+    <script src="../assets/js/jquery.sticky.js"></script>
+    
+    <!-- counter , waypoint,Hover Direction -->
+    <script src="../assets/js/jquery.counterup.min.js"></script>
+    <script src="../assets/js/waypoints.min.js"></script>
+    <script src="../assets/js/jquery.countdown.min.js"></script>
+    <script src="../assets/js/hover-direction-snake.min.js"></script>
+
+    <!-- contact js -->
+    <script src="../assets/js/contact.js"></script>
+    <script src="../assets/js/jquery.form.js"></script>
+    <script src="../assets/js/jquery.validate.min.js"></script>
+    <script src="../assets/js/mail-script.js"></script>
+    <script src="../assets/js/jquery.ajaxchimp.min.js"></script>
+    
+    <!-- Jquery Plugins, main Jquery -->	
+    <script src="../assets/js/plugins.js"></script>
+    <script src="../assets/js/main.js"></script>
      <script src="login.js"></script>
 </body>
 </html>
