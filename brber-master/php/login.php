8ef9fae9cd06ffadddf3c93150728c0ae5b3f3ca
ConstantinosConstantinou <kong.konstantinou@edu.cut.ac.cy>
2024-03-18 15:19:10 +0200

    diorthosi password hide unhide

--- brber-master/php/login.php
+++ brber-master/php/login.php
@@ -144,8 +144,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
           <div class="form-group">
                <label for="exampleInputPassword1">Password</label>
 
-               <input type="password" name="password" id="password"  class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?><?php echo (!empty($login_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>" placeholder="Password"/>
-               <i class="bi bi-eye-slash" id="togglePassword" ></i>          
+               <input type="password" name="password" id="password"  class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?><?php echo (!empty($login_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>" placeholder="Password"/>          
                
                <span class="invalid-feedback"><?php echo $login_err; ?>
                <span class="invalid-feedback"><?php echo $password_err; ?> 
