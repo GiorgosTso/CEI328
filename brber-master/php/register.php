a5f046d801f7a0957626d0e95c196a0780cb6080
ConstantinosConstantinou <kong.konstantinou@edu.cut.ac.cy>
2024-03-25 14:04:00 +0200

    Revert "reverting changes"
    This reverts commit dac285bd83e58710dd68fb0ef316cdefe1370d1b.

--- brber-master/php/register.php
+++ brber-master/php/register.php
@@ -1,23 +1,22 @@
 <?php
 
 session_start();
-include("connection.php");
-
-$name = $_POST['name'];
-$surname = $_POST['surname'];
-$city = $_POST['city'];
-$email = $_POST['email'];
-$area = $_POST['area'];
-$phone = $_POST['phone'];
-$username = $_POST['username'];
-$typeOfUser = $_POST['typeOfUser'];
-$password = $_POST['password'];
-
-$hash = password_hash ($password, PASSWORD_DEFAULT);
+include("config.php");
 
 
 if($_SERVER['REQUEST_METHOD'] == "POST")
 {
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $city = $_POST['city'];
+    $email = $_POST['email'];
+    $area = $_POST['area'];
+    $phone = $_POST['phone'];
+    $username = $_POST['username'];
+    //$typeOfUser = $_POST['typeOfUser'];
+    $password = $_POST['password'];
+    
+    $hash = password_hash ($password, PASSWORD_DEFAULT);
     $email = $_POST['email'];
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -26,16 +25,17 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
     {
        
         //save to database
-        $query = "insert into clients (name,surname,city,email,area,phone) 
-        values ('$name','$surname','$city','$email','$area','$phone')";
+        //$query = "insert into clients (name,surname,city,email,area,phone) 
+        //values ('$name','$surname','$city','$email','$area','$phone')";
 
         $query1 = "insert into useraccount(username, password, typeOfUser)values ('$username', '$hash', '3')";
 
-        $result = mysqli_query($connection, $query);
+        //$result = mysqli_query($conn, $query);
 
-        $result1 = mysqli_query($connection, $query1);
+        $result1 = mysqli_query($conn, $query1);
         
-        header("Location: ../html/login.php?Success=Account created successfully");
+        header("Location: ../php/login.php?Success=Account created successfully");
+
         die();
     }else
     {
