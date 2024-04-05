40cd3e14a1fbf7f1dd045f41484c4b18e928ec1f
panaischr <chrpanais@gmail.com>
2024-04-05 20:44:28 +0300

    Update review.php

--- brber-master/html/review.php
+++ brber-master/html/review.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<?php include "header.php"; ?>
 <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title> Barber HTML-5 Template </title>
@@ -173,59 +174,7 @@
     }
 </style>
 </head>
-
-        <!--? Header Start -->
-        <header>
-        <!--? Header Start -->
-        <div class="header-area header-transparent pt-20">
-            <div class="main-header header-sticky">
-                <div class="container-fluid">
-                    <div class="row align-items-center">
-                        <!-- Logo -->
-                        <div class="col-xl-2 col-lg-2 col-md-1">
-                            <div class="logo">
-                                <a href="../html/index.php"><img src="../assets/img/logo.png" alt=""></a>
-                            </div>
-                        </div>
-                        <div class="col-xl-10 col-lg-10 col-md-10">
-                            <div class="menu-main d-flex align-items-center justify-content-end">
-                                <!-- Main-menu -->
-                                <div class="main-menu f-right d-none d-lg-block">
-                                    <nav>
-                                        <ul id="navigation">
-                                            <li class=""><a href="../html/index.php">Home</a></li>
-                                            <li><a href="../html/about.php">About</a></li>
-                                            <li><a href="../html/services.php">Services</a></li>
-                                            <li><a href="../cart/index.php">Orders</a></li>
-                                            <li><a href="../html/appointment.php">Appointments</a></li>
-
-                                            <li class = ""><a href="../html/contact.php">Contact</a></li>
-                                            <li><a href="../html/review.php">Reviews</a></li>
-                                        </ul>
-                                    </nav>
-                                </div>
-                                <div class="header-right-btn f-right d-none d-lg-block ml-30">
-                                    <a href="../php/login.php" class="btn header-btn">Login</a>
-                                </div>
-                                <div class="header-right-btn f-right d-none d-lg-block ml-30">
-                                    <a href="register.php" class="btn header-btn">Sign Up</a>
-                                </div>
-                            </div>
-                        </div>   
-                        <!-- Mobile Menu -->
-                        <div class="col-12">
-                            <div class="mobile_menu d-block d-lg-none"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Header End -->
-    </header>
-    
-     
 <body>
-
 <div class="container">
     <div class="review-form">
         <h2>Leave a Review</h2>
