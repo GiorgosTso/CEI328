ede7dda315871841aa345af2f3ce15018b501310
panaischr <chrpanais@gmail.com>
2024-04-17 15:22:51 +0300

    for now?

--- brber-master/html/footer.php
+++ brber-master/html/footer.php
@@ -1,49 +1,57 @@
-<div class="footer-area section-bg" data-background="../assets/img/gallery/footer_bg.png">
-            <div class="container">
-                <div class="footer-top footer-padding">
+<head>
+    <style> 
+   .footer-area {
+        height: 590px;
+    }
+    </style>
+
+    <div class="footer-area section-bg" data-background="../assets/img/gallery/footer_bg.png">
+    <div class="container">
+        <div class="footer-top footer-padding">
                     <div class="row d-flex justify-content-between">
                         <div class="col-xl-3 col-lg-4 col-md-5 col-sm-8">
                             <div class="single-footer-caption mb-50">
                                 <!-- logo -->
-                                <div class="footer-logo">
+                                <div class="footer-logo" style="position: absolute;">
                                     <a href="../html/index.php" style="text-decoration: none;"><img src="../assets/img/logo.png" alt=""></a>
+                                    <div class="footer-number">
+                                    <br>
+                                    <h4><span>+357 </span>24 044146</h4>
+                                    <br>
+                                    <p>michalis@hotmail.com</p>
+                                </div>
                                 </div>
                                 <div class="footer-tittle">
                                     <div class="footer-pera">
-                                        <p class="info1">Receive updates and latest news direct from Simply enter.</p>
                                     </div>
-                                </div>
-                                <div class="footer-number">
-                                    <h4><span>+357 </span>24 044146</h4>
-                                    <p>michalis@hotmail.com</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-xl-2 col-lg-2 col-md-3 col-sm-5">
                             <div class="single-footer-caption mb-50">
                                 <div class="footer-tittle">
-                                    <h4>Location </h4>
+                                    <!-- <h4>Location </h4>
                                     <ul>
                                         <li><a href="#">Advanced</a></li>
                                         <li><a href="#"> Management</a></li>
                                         <li><a href="#">Corporate</a></li>
                                         <li><a href="#">Customer</a></li>
                                         <li><a href="#">Information</a></li>
-                                    </ul>
+                                    </ul> -->
                                 </div>
                             </div>
                         </div>
                         <div class="col-xl-2 col-lg-2 col-md-3 col-sm-5">
                             <div class="single-footer-caption mb-50">
                                 <div class="footer-tittle">
-                                    <h4>Explore</h4>
+                                    <!-- <h4>Explore</h4>
                                     <ul>
                                         <li><a href="#">Cookies</a></li>
                                         <li><a href="#">About</a></li>
                                         <li><a href="#">Privacy Policy</a></li>
                                         <li><a href="#">Proparties</a></li>
                                         <li><a href="#">Licenses</a></li>
-                                    </ul>
+                                    </ul> -->
                                 </div>
                             </div>
                         </div>
@@ -66,45 +74,13 @@
                                                     </ul>
                                                 </div>
                                             </div>
-                                    </div>
-                                        <p class="info1">Subscribe now to get daily updates</p>
-                                    </div>
-                                </div>
-                                <!-- Form -->
-                                <div class="footer-form">
-                                    <div id="mc_embed_signup">
-                                        <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative mail_part" novalidate="true">
-                                            <input type="email" name="EMAIL" id="newsletter-form-email" placeholder=" Email Address " class="placeholder hide-on-focus" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your email address'">
-                                            <div class="form-icon">
-                                                <button type="submit" name="submit" id="newsletter-submit" class="email_icon newsletter-submit button-contactForm">Send</button>
-                                            </div>
-                                            <div class="mt-10 info"></div>
-                                        </form>
-                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="footer-bottom">
-                    <div class="row d-flex justify-content-between align-items-center">
-                        <div class="col-xl-9 col-lg-8">
-                            <div class="footer-copy-right">
-                                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4">
-                            <!-- Footer Social -->
-                            <div class="footer-social f-right">
-                                <a href="#" style="text-decoration: none;"><i class="fab fa-twitter"></i></a>
-                                <a href="https://www.facebook.com/sai4ull" style="text-decoration: none;"><i class="fab fa-facebook-f"></i></a>
-                                <a href="#" style="text-decoration: none;"><i class="fas fa-globe"></i></a>
-                                <a href="#" style="text-decoration: none;"><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <!-- <div class="footer-bottom">
+                </div> -->
             </div>
         </div>
+</head>
