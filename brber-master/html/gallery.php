307c5a50cf7e606f05d6135b8bd8a654b137fd81
AndreasChr03 <andreasggchristou@gmail.com>
2024-05-09 18:33:07 +0300

    telos tou gallery

--- brber-master/html/gallery.php
+++ brber-master/html/gallery.php
@@ -1,3 +1,33 @@
+<?php 
+	include "../php/config.php";
+	
+	
+	if (isset($_FILES["fileToUpload"]) && $_FILES["fileToUpload"]["error"] == 0) {
+		$target_dir = "../assets/img/gallery/"; // Change this to the desired directory for uploaded files
+		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		$file_type = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+		$id = $_POST['number'];
+
+		// Check if the file is allowed (you can modify this to allow specific file types)
+		$allowed_types = array("jpg", "jpeg", "png", "pdf");
+		if (!in_array($file_type, $allowed_types)) {
+			echo "Sorry, only JPG, JPEG, PNG and PDF files are allowed.";
+		} else {
+			// Move the uploaded file to the specified directory
+			if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+				// File upload success, now store information in the database
+				$filename = $_FILES["fileToUpload"]["name"];
+				$sql = "UPDATE gallery SET image = ? WHERE id = ?";
+				$stmt = $conn->prepare($sql);
+				$stmt-> bind_param("si", $target_file,$id);
+				$stmt->execute();
+				$result = $stmt->get_result();
+			}
+		}
+	}
+?>
+
+
 <!DOCTYPE html>
 <html class="no-js" lang="zxx">
   <head>
@@ -42,26 +72,10 @@
     </div>
     <!-- Preloader Start -->
     <?php include "header.php";
-        include "../php/config.php";
+        
     ?>
     <div class="gallery">
-        <?php 
-        //Fetch image paths from the database
-        
-        // $db = new PDO('sqlite:southside_db');
-        // $stmt = $db->prepare("SELECT image_id, image_path, caption FROM gallery");
-        // $stmt->execute();
-        // $images = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        foreach ($images as $image) {
-            echo '<div class="gallery-item">';
-            echo '<img src="' . htmlspecialchars($image['image_path']) . '" alt="Gallery Image">';
-            if ($image['caption']) {
-                echo '<p class="image-caption">' . htmlspecialchars($image['caption']) . '</p>';
-            }
-            echo '</div>';
-        }
-        ?>
+      
     </div>
     <main>
       <!--? Hero Start -->
@@ -92,67 +106,168 @@
                             <?php 
                         if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true  && $_SESSION['typeOfUser'] == '1'){
                         // Display the button for admin
-                        echo'<a href="admin_upload.php"> <button type="button" class="btn btn-info btn-round" data-toggle="modal" data-target="#loginModal" style="margin-top: 80px;">
-                        Add image
-                        </button>
-                        </a>';} 
+                        echo'<button type="button" class="btn btn-info btn-round" data-toggle="modal" data-target="#exampleModal" style="margin-top: 80px;">
+                                Edit Images
+							</button>';} 
                         ?>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 col-sm-6">
-                        <div class="box snake mb-30">
-                            <div class="gallery-img " style="background-image: url(../assets/img/cut10.jpg);"></div>
-                            <div class="overlay"></div>
-                        </div>
-                    </div>
-                    <div class="col-lg-8 col-md-6 col-sm-6">
-                        <div class="box snake mb-30">
-                            <div class="gallery-img " style="background-image: url(../assets/img/barber1.jpg);"></div>
-                            <div class="overlay"></div>
-                        </div>
-                    </div>
-                    <div class="col-lg-8 col-md-6 col-sm-6">
-                        <div class="box snake mb-30">
-                            <div class="gallery-img" style="background-image: url(../assets/img/barber.jpg);"></div>
-                            <div class="overlay"></div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-6">
-                        <div class="box snake mb-30">
-                            <div class="gallery-img " style="background-image: url(../assets/img/cut8.jpg);"></div>
-                            <div class="overlay"></div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="box snake mb-30">
-                            <div class="gallery-img " style="background-image: url(../assets/img/cut13.jpg);"></div>
-                            <div class="overlay"></div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="box snake mb-30">
-                            <div class="gallery-img " style="background-image: url(../assets/img/cut14.jpg);"></div>
-                            <div class="overlay"></div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="box snake mb-30">
-                            <div class="gallery-img " style="background-image: url(../assets/img/cut9.jpg);"></div>
-                            <div class="overlay"></div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="box snake mb-30">
-                            <div class="gallery-img " style="background-image: url(../assets/img/cut12.jpg);"></div>
-                            <div class="overlay"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				  <div class="modal-dialog modal-lg" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <h5 class="modal-title" id="exampleModalLabel">Add Image</h5>
+				        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				          <span aria-hidden="true">&times;</span>
+				        </button>
+				      </div>
+				      <div class="modal-body">
+				        <!-- Form to upload image -->
+						<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+				          <div class="form-group">
+				            <label for="imageUpload" class="mb-2">Select image to upload:</label>
+				            <div class="custom-file">
+				              <input type="file" class="custom-file-input" id="imageUpload" name="fileToUpload" required>
+				              <label class="custom-file-label" for="imageUpload">Choose file...</label>
+				            </div>
+				          </div>
+				
+				          <!-- Dropdown to select number -->
+				          <div class="form-group">
+				            <label for="numberSelect" class="mb-2">Choose the number of the image:</label>
+				            <select class="form-control" id="numberSelect" name="number">
+				              <?php for ($i = 1; $i <= 8; $i++): ?>
+				                <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+				              <?php endfor; ?>
+				            </select>
+				          </div>
+
+				          <div class="modal-footer">
+				            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				            <button type="submit" class="btn btn-primary">Upload Image</button>
+				          </div>
+				        </form>
+				    </div>
+				</div>
+			</div>
+		</div>
+            <?php    
+		$sql = "SELECT * FROM gallery";
+$select_stmt = $conn->prepare($sql);
+if ($select_stmt === false) {
+    echo "Error preparing the statement: " . $conn->error;
+} else {
+    // Execute the statement
+    if ($select_stmt->execute()) {
+        $result = $select_stmt->get_result();
+
+        // Organize images by their ID for easy reference
+        $images = [];
+        while ($row = $result->fetch_assoc()) {
+            $images[$row['id']] = $row['image'];
+        }
+    } else {
+        echo "Error executing the statement: " . $select_stmt->error;
+    }
+    $select_stmt->close();
+}
+
+// Define the path to the gallery images and the default image
+$target_dir = "../assets/img/gallery/";
+$default_image = '../assets/img/logo.png';
+?>
+
+<div class="row">
+    <!-- Image for ID 1 -->
+    <div class="col-lg-4 col-md-6 col-sm-6">
+        <div class="box snake mb-30">
+            <div class="gallery-img" style="background-image: url('<?php echo isset($images[1]) ? htmlspecialchars($images[1]) : $default_image; ?>');"></div>
+            <div class="overlay"></div>
         </div>
-        <!-- Gallery Area End -->
+    </div>
+
+    <!-- Image for ID 2 -->
+    <div class="col-lg-8 col-md-6 col-sm-6">
+        <div class="box snake mb-30">
+            <div class="gallery-img" style="background-image: url('<?php echo isset($images[2]) ? htmlspecialchars($images[2]) : $default_image; ?>');"></div>
+            <div class="overlay"></div>
+        </div>
+    </div>
+
+    <!-- Image for ID 3 -->
+    <div class="col-lg-8 col-md-6 col-sm-6">
+        <div class="box snake mb-30">
+            <div class="gallery-img" style="background-image: url('<?php echo isset($images[3]) ? htmlspecialchars($images[3]) : $default_image; ?>');"></div>
+            <div class="overlay"></div>
+        </div>
+    </div>
+
+    <!-- Image for ID 4 -->
+    <div class="col-lg-4 col-md-6 col-sm-6">
+        <div class="box snake mb-30">
+            <div class="gallery-img" style="background-image: url('<?php echo isset($images[4]) ? htmlspecialchars($images[4]) : $default_image; ?>');"></div>
+            <div class="overlay"></div>
+        </div>
+    </div>
+
+    <!-- Image for ID 5 -->
+    <div class="col-lg-6 col-md-6 col-sm-6">
+        <div class="box snake mb-30">
+            <div class="gallery-img" style="background-image: url('<?php echo isset($images[5]) ? htmlspecialchars($images[5]) : $default_image; ?>');"></div>
+            <div class="overlay"></div>
+        </div>
+    </div>
+
+    <!-- Image for ID 6 -->
+    <div class="col-lg-6 col-md-6 col-sm-6">
+        <div class="box snake mb-30">
+            <div class="gallery-img" style="background-image: url('<?php echo isset($images[6]) ? htmlspecialchars($images[6]) : $default_image; ?>');"></div>
+            <div class="overlay"></div>
+        </div>
+    </div>
+
+    <!-- Image for ID 7 -->
+    <div class="col-lg-6 col-md-6 col-sm-6">
+        <div class="box snake mb-30">
+            <div class="gallery-img" style="background-image: url('<?php echo isset($images[7]) ? htmlspecialchars($images[7]) : $default_image; ?>');"></div>
+            <div class="overlay"></div>
+        </div>
+    </div>
+
+    <!-- Image for ID 8 -->
+    <div class="col-lg-6 col-md-6 col-sm-6">
+        <div class="box snake mb-30">
+            <div class="gallery-img" style="background-image: url('<?php echo isset($images[8]) ? htmlspecialchars($images[8]) : $default_image; ?>');"></div>
+            <div class="overlay"></div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6">
+        <div class="box snake mb-30">
+            <div class="gallery-img" style="background-image: url('<?php echo isset($images[9]) ? htmlspecialchars($images[8]) : $default_image; ?>');"></div>
+            <div class="overlay"></div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6">
+        <div class="box snake mb-30">
+            <div class="gallery-img" style="background-image: url('<?php echo isset($images[10]) ? htmlspecialchars($images[8]) : $default_image; ?>');"></div>
+            <div class="overlay"></div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6">
+        <div class="box snake mb-30">
+            <div class="gallery-img" style="background-image: url('<?php echo isset($images[11]) ? htmlspecialchars($images[8]) : $default_image; ?>');"></div>
+            <div class="overlay"></div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6">
+        <div class="box snake mb-30">
+            <div class="gallery-img" style="background-image: url('<?php echo isset($images[12]) ? htmlspecialchars($images[8]) : $default_image; ?>');"></div>
+            <div class="overlay"></div>
+        </div>
+    </div>
+    
+</div>
     </main>
 
     <footer>
@@ -162,7 +277,15 @@
     <div id="back-top">
       <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
     </div>
-
+	<script>
+// Script to update the label of the custom file input
+$(document).ready(function () {
+  $('.custom-file-input').on('change', function() { 
+     let fileName = $(this).val().split('\\').pop(); 
+     $(this).next('.custom-file-label').addClass("selected").html(fileName);
+  });
+});
+</script>
     <!-- JS here -->
 
     <script src="../assets/js/vendor/modernizr-3.5.0.min.js"></script>
